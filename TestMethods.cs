b65b521e08ae8dab10e635301c6296b8a1a5c703
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2023-08-19 23:16:57 -0500

    punto 1 - GetNextGreaterValue

--- TestMethods.cs
+++ TestMethods.cs
@@ -17,6 +17,40 @@ namespace TestProject1
         {
             Stack<int> result = null;
 
+            if (sourceStack.Count > 0 && sourceStack != null)
+            {
+                int[] tmp = new int[sourceStack.Count];
+                for (int i = sourceStack.Count; i > 0 ; i--)
+                {
+                    tmp[i] = sourceStack.Pop();
+                }
+
+                for (int i = 0; i < sourceStack.Count; i++)
+                {
+                    if(i == sourceStack.Count - 1)
+                    {
+                        result.Push(-1);
+                    }
+                    else
+                    {
+                        bool gate = false;
+                        for (int j = i + 1; j < sourceStack.Count - i; j++)
+                        {
+                            if (tmp[i] < tmp[j])
+                            {
+                                result.Push(tmp[j]);
+                                gate = true;
+                                break;
+                            }
+                        }
+                        if (!gate)
+                        {
+                            result.Push(-1);
+                        }
+                    }
+                }
+            }
+
             return result;
         }
 
