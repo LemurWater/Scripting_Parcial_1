99ce42d83e94a7b391e1fcd16547cdfd3482d1bd
WaterLemur <38868316+LemurWater@users.noreply.github.com>
2023-08-21 15:54:12 -0500

    desarrollo parcial 1
    desarrollo del parcial #1 de Scripting

--- TestMethods.cs
+++ TestMethods.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -20,39 +23,37 @@ namespace TestProject1
         {
             Stack<int> result = new();
 
-            if (sourceStack.Count > 0 && sourceStack != null)
+            if(sourceStack.Count > 0 && sourceStack != null)
             {
                 int[] tmp = new int[sourceStack.Count];
-                for (int i = sourceStack.Count - 1; i > 0 ; i--)
+
+                for (int i = sourceStack.Count; i > 0; i--)
                 {
-                    tmp[i] = sourceStack.Pop();
+                    tmp[i-1] = sourceStack.Pop();
                 }
 
-                for (int i = 0; i < sourceStack.Count; i++)
+                for (int i = 0; i < tmp.Length; i++)
                 {
-                    if(i == sourceStack.Count - 1)
+                    if(i == tmp.Length - 1)
                     {
                         result.Push(-1);
+                        break;
                     }
-                    else
+
+                    bool gate = false;
+                    for (int j = i+1; j < tmp.Length; j++)
                     {
-                        bool gate = false;
-                        for (int j = i + 1; j < sourceStack.Count - i; j++)
+                        if (tmp[i] < tmp[j])
                         {
-                            if (tmp[i] < tmp[j])
-                            {
-                                result.Push(tmp[j]);
-                                gate = true;
-                                break;
-                            }
-                        }
-                        if (!gate)
-                        {
-                            result.Push(-1);
+                            result.Push(tmp[j]);
+                            gate = true;
+                            break;
                         }
                     }
+                    if (!gate) result.Push(-1);
                 }
             }
+            else result.Push(-1);
 
             return result;
         }
@@ -98,17 +99,23 @@ namespace TestProject1
             if (sourceDict.Count > 0 && sourceDict != null)
             {
                 int[] tmp = new int[sourceDict.Count];
+                int[] sorted = new int[sourceDict.Count];
                 int i = 0;
                 foreach (KeyValuePair<int, EValueType> kvp in sourceDict)
                 {
                     tmp[i] = kvp.Key;
                     i++;
                 }
-                //
                 tmp = BubbleSort(tmp);
-                //
-                result =  FillDictionaryFromSource(tmp);
+                i = 0;
+                for (int j = tmp.Length - 1; j >= 0; j--)
+                {
+                    sorted[j] = tmp[i];
+                    i++;
+                }
+                result = FillDictionaryFromSource(sorted);
             }
+            else result.Add(1, EValueType.Prime);
 
             return result;
         }
@@ -142,7 +149,7 @@ namespace TestProject1
                     }
                 }
             }
-            return null;
+            return array;
         }
         internal static Queue<Ticket>[] ClassifyTickets(List<Ticket> sourceList)
         {
@@ -150,38 +157,111 @@ namespace TestProject1
 
             if (sourceList.Count > 0 && sourceList != null)
             {
-                Queue<Ticket> payment = new Queue<Ticket>();
-                Queue<Ticket> subscription = new Queue<Ticket>();
-                Queue<Ticket> cancellation = new Queue<Ticket>();
+                Queue<Ticket> payments = new();
+                Queue<Ticket> subscriptions = new();
+                Queue<Ticket> cancellations = new();
 
-                for (int i = 0; i < sourceList.Count - 1; i++)
+                List<Ticket> l_payment = new();
+                List<Ticket> l_subscription = new();
+                List<Ticket> l_cancellation = new();
+                foreach (Ticket t in sourceList)
                 {
-                    switch (sourceList[i].RequestType)
+                    switch (t.RequestType)
                     {
-                        case Ticket.ERequestType.Payment: payment.Enqueue(sourceList[i]); break;
-                        case Ticket.ERequestType.Subscription: subscription.Enqueue(sourceList[i]); break;
-                        case Ticket.ERequestType.Cancellation: cancellation.Enqueue(sourceList[i]); break;
+                        case Ticket.ERequestType.Payment: l_payment.Add(t); break;
+                        case Ticket.ERequestType.Subscription: l_subscription.Add(t); break;
+                        case Ticket.ERequestType.Cancellation: l_cancellation.Add(t); break;
                     }
                 }
-                result[0] = payment;
-                result[1] = subscription;
-                result[2] = cancellation;
+                //
+                for (global::System.Int32 i = 0; i < 3; i++)
+                {
+                    List<Ticket> tmp = new();
+                    switch (i)
+                    {
+                        case 0: tmp = l_payment; break;
+                        case 1: tmp = l_subscription; break;
+                        case 2: tmp = l_cancellation; break;
+                    }
+                    Ticket[] t_array = BubbleSort(ListToArray(tmp));
+                    foreach (Ticket ticket in t_array)
+                    {
+                        switch (i)
+                        {
+                            case 0: payments.Enqueue(ticket); break;
+                            case 1: subscriptions.Enqueue(ticket); break;
+                            case 2: cancellations.Enqueue(ticket); break;
+                        }
+                    }
+                }
+
+                result[0] = payments;
+                result[1] = subscriptions;
+                result[2] = cancellations;
             }
 
             return result;
         }
+        internal static Ticket[] ListToArray(List<Ticket> tickets)
+        {
+            Ticket[] result = new Ticket[tickets.Count];
+            for (int i = 0; i < tickets.Count; i++)
+            {
+                result[i] = tickets[i];
+            }
+            return result;
+        }
+        internal static Ticket[] BubbleSort(Ticket[] array)// Ticket edition
+        {
+            if (array.Length > 0 && array != null)
+            {
+                int n = array.Length;
+                bool swapped;
 
+                for (int i = 0; i < n - 1; i++)
+                {
+                    swapped = false;
+
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (array[j].Turn > array[j + 1].Turn)
+                        {
+                            // Swap array[j] and array[j + 1]
+                            Ticket temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+
+                    // If no two elements were swapped by inner loop, then the array is already sorted
+                    if (!swapped)
+                    {
+                        break;
+                    }
+                }
+            }
+            return array;
+        }
         internal static bool AddNewTicket(Queue<Ticket> targetQueue, Ticket ticket)
         {
-            bool result = false;
+            if(targetQueue == null) return false;
+            if (ticket.RequestType != targetQueue.Peek().RequestType) return false;
 
-            if (ticket.RequestType == targetQueue.ElementAt(0).RequestType)
+            if(ticket.Turn > 99)
+            {
+                /*
+                int turn = (ticket.Turn % 99) + 1;
+                Ticket t = new Ticket(ticket.RequestType, turn);
+                targetQueue.Enqueue(t);
+                */
+                return false;
+            }
+            else
             {
                 targetQueue.Enqueue(ticket);
-                result = true;
+                return true;
             }
-
-            return result;
         }        
     }
 }
